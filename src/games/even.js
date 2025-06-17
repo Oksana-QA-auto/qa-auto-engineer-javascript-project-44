6124e5903632a75488569a1cabe30f8f8b6dbe02
Oksana Auto-QA <oksana.zhornova@gmail.com>
2025-06-17 18:45:50 +0500

    Fix: correct import of greetUser from cli.js

--- src/games/even.js
+++ src/games/even.js
@@ -1,5 +1,5 @@
 import readlineSync from 'readline-sync';
-import { greetUser } from '../cli.js';
+import greetUser from '../cli.js';
 
 // Предикат: возвращает true, если число чётное
 const isEven = (num) => num % 2 === 0;
