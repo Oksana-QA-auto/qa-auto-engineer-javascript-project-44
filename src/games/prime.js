7dd25ae6fb79b1f45f731fd31a1c3990201db21c
Oksana Auto-QA <oksana.zhornova@gmail.com>
2025-06-24 13:13:57 +0500

    Добавлена игра Простое ли число

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,22 +1,26 @@
 import runGame from '../index.js'
 
+const description = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+
 const isPrime = (num) => {
-  if (num < 2) return false
-  for (let i = 2; i <= Math.sqrt(num); i++) {
-    if (num % i === 0) return false
+  if (num < 2) {
+    return false
+  }
+  for (let i = 2; i <= Math.sqrt(num); i += 1) {
+    if (num % i === 0) {
+      return false
+    }
   }
   return true
 }
 
-const getRoundData = () => {
+const generateRound = () => {
   const number = Math.floor(Math.random() * 100) + 1
+  const question = `${number}`
   const correctAnswer = isPrime(number) ? 'yes' : 'no'
-  return [number.toString(), correctAnswer]
+  return [question, correctAnswer]
 }
 
-const runPrimeGame = () => {
-  const rules = 'Answer "yes" if given number is prime. Otherwise answer "no".'
-  runGame(rules, getRoundData)
-}
+const playPrimeGame = () => runGame(description, generateRound)
 
-export default runPrimeGame
+export default playPrimeGame
