2e3bee2255cadc4ebe2751c101fd61fa834e689f
Oksana Auto-QA <oksana.zhornova@gmail.com>
2025-06-27 19:07:01 +0500

    Настроен ESLint: обновлен config. и зависимости

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,23 @@
-import js from '@eslint/js'
+import * as js from '@eslint/js'
 import globals from 'globals'
+import * as stylistic from '@stylistic/eslint-plugin'
 import { defineConfig } from 'eslint/config'
 
 export default defineConfig([
-  { files: ['**/*.{js,mjs,cjs}'], plugins: { js }, extends: ['js/recommended'] },
-  { files: ['**/*.js'], languageOptions: { sourceType: 'script' } },
-  { files: ['**/*.{js,mjs,cjs}'], languageOptions: { globals: globals.node } },
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    languageOptions: {
+      sourceType: 'script',
+      globals: globals.node,
+    },
+    plugins: {
+      js,
+      stylistic,
+    },
+    rules: {
+      'quotes': ['error', 'single'],
+      'semi': ['error', 'never'],
+      'no-multiple-empty-lines': ['error', { max: 0 }],
+    },
+  },
 ])
